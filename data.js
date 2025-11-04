f1cd12f9c76018fcab007fee26b03c803167834b
dvsp3639 <prasad.dam30@gmail.com>
2025-11-04 16:41:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+// credentials.js - User credentials for Orange Expense Tracker
+const USER_CREDENTIALS = [
+    { username: "cashier1", password: "marshmello", role: "data_entry", name: "cashier1" },
+    { username: "eswari", password: "davidoff", role: "data_entry", name: "Eswari" },
+    { username: "cashier2", password: "marigold", role: "data_entry", name: "cashier2" },
+    { username: "teja", password: "lavender", role: "approver", name: "Sai Teja" },
+    { username: "swathi", password: "sunflower", role: "approver", name: "Swathi" },     
+    { username: "praveen", password: "cupcake", role: "approver", name: "Praveen" },
+    { username: "prasad", password: "honeycomb", role: "approver", name: "Kalyan Prasad" },
+    { username: "viewer", password: "password", role: "viewer", name: "Viewer" }
+];
