e527d1fb4205825ad760e0d3acb668b760fef917
dvsp3639 <prasad.dam30@gmail.com>
2025-11-04 16:53:02 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,8 @@ const USER_CREDENTIALS = [
     { username: "cashier1", password: "marshmello", role: "data_entry", name: "cashier1" },
     { username: "eswari", password: "davidoff", role: "data_entry", name: "Eswari" },
     { username: "cashier2", password: "marigold", role: "data_entry", name: "cashier2" },
+    { username: "satish", password: "pumpkin", role: "data_entry", name: "Satish" },
+    { username: "venkatesh", password: "moringa", role: "data_entry", name: "Venkatesh" },
     { username: "teja", password: "lavender", role: "approver", name: "Sai Teja" },
     { username: "swathi", password: "sunflower", role: "approver", name: "Swathi" },     
     { username: "praveen", password: "cupcake", role: "approver", name: "Praveen" },
